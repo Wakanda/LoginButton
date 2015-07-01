b44b71b4810b829f9011716cf06ad926c5eba83f
Stéphane Puybareau <stephane.puybareau@4d.com>
2015-07-01 11:22:19 +0200

    Logout function

--- widget.js
+++ widget.js
@@ -3,9 +3,19 @@ WAF.define('LoginButton', ['waf-core/widget'], function(widget) {
     "use strict";
 
     var login = widget.create('LoginButton', {
-    	tagName: 'button',
-    	titleLogin: widget.property({ type: 'string', defaultValue: 'Login' }),
-    	titleLogout: widget.property({ type: 'string', defaultValue: 'Logout' }),
+        tagName: 'button',
+        mode: widget.property({
+            type: 'enum',
+            values: {
+                both: 'Both',
+                login: 'Login only',
+                logout: 'Logout only'
+            },
+            defaultValue: 'both'
+        }),
+        verify: widget.property({ type: 'boolean', defaultValue: true }),
+        titleLogin: widget.property({ type: 'string', defaultValue: 'Login' }),
+        titleLogout: widget.property({ type: 'string', defaultValue: 'Logout' }),
         redirectUrl: widget.property({
             type: 'string',
             description: "If the login is successful, the page will be redirected to this url",
@@ -31,20 +41,25 @@ WAF.define('LoginButton', ['waf-core/widget'], function(widget) {
             var that = this;
 
             if (!WAF.directory) return;
-            
+
             var loginElementID = this.loginField() || null;
             var pwdElementID = this.passwordField() || null;
             if (loginElementID === null || pwdElementID === null) return;
-     
+
             var loginElement = $$(loginElementID);
             var pwdElement = $$(pwdElementID);
-               
+
             if ((!loginElement.value || !pwdElement.value)) return;
 
             var userName = loginElement.value() || null;
             var password = pwdElement.value() || null;
 
-            if (userName === null || password === null) return;
+            if (userName === null || password === null) {
+            	that.user = null;
+                that.fire('error', {event:event, user: userName});
+                that.render();
+                return;
+            }
 
             WAF.directory.login({
                 onSuccess:function(event) {
@@ -62,7 +77,7 @@ WAF.define('LoginButton', ['waf-core/widget'], function(widget) {
 
                     that.render();
                 }
-            }, userName, password);            
+            }, userName, password);
         },
         _logout: function() {
             var that = this;
@@ -74,47 +89,69 @@ WAF.define('LoginButton', ['waf-core/widget'], function(widget) {
                     if (rurl) {
                         document.location.href = rurl;
                     } else {
-                        that.checkState();    
+                        that.checkState();
                     }
                 }
             });
         },
-        checkState: function() {
+        checkState: function(startup) {
             var that = this;
-            WAF.directory.currentUser({
-                onSuccess: function(event) {
-                    that.user = event.result;
-                    that.render();
-                }
-            });
+            if (WAF.directory) {
+                WAF.directory.currentUser({
+                    onSuccess: function(event) {
+                        that.user = event.result;
+                        that.render();
+                        if (startup) {
+                            var rurl = that.user ? that.redirectUrl() : that.redirectUrlLogout();
+                            if (rurl) {
+                                document.location.href = rurl;
+                            }
+                        }
+                    }
+                });
+            }
         },
-		render: function() {
-			this.node.innerHTML = '<span>' + (this.user ? this.titleLogout() : this.titleLogin()) + '</span>';
-		},
+        render: function() {
+            switch(this.mode()) {
+                case 'login':
+                    this.node.innerHTML = '<span>' + this.titleLogin() + '</span>';
+                    this.node.disabled = this.user;
+                    break;
+                case 'logout':
+                    this.node.innerHTML = '<span>' + this.titleLogout() + '</span>';
+                    this.node.disabled = !this.user;
+                    break;
+                default:
+                    this.node.innerHTML = '<span>' + (this.user ? this.titleLogout() : this.titleLogin()) + '</span>';
+                    break;
+            }
+        },
         init: function() {
 
             var that = this;
 
-			this.render();
+            this.render();
             this.titleLogin.onChange(this.render);
             this.titleLogout.onChange(this.render);
+            this.mode.onChange(this.render);
 
-            this.addClass('btn btn-default');
-            
+
             this.user = null;
 
-            this.checkState();
-             
-            $(this.node).on('click', function() {
-                if (that.user === null) {
-                    that._login();
-                } else {
-                    that._logout();
-                }
-            });
+            this.checkState(this.verify());
+
+            $(this.node).on('click', this.click.bind(this));
+        },
+        click: function() {
+            if (this.user === null) {
+                this._login();
+            } else {
+                this._logout();
+            }
         }
     });
-            
+    login.addClass('btn btn-default');
+
     return login;
 
 });
